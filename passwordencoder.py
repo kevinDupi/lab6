a83b81e8b74248c51dadbef49ed0c8e9037bed06
kevinDupi <dupignyk@ufl.edu>
2024-10-23 16:31:42 -0400

    Lab 6 code

---
+++ passwordencoder.py
@@ -0,0 +1,44 @@
+def encode(password):
+    pList = list(password)
+    for letter in range(len(pList)):
+        pList[letter] = str(int(pList[letter]) + 3)
+
+    finalPassword = "".join(pList)
+    return finalPassword
+
+def decode(password):
+    pList = list(password)
+    for letter in range(len(pList)):
+        pList[letter] = str(int(pList[letter]) - 3)
+
+
+    finalPassword = "".join(pList)
+    return finalPassword
+
+def main():
+    password = ''
+
+    while True:
+        print('Menu')
+        print('------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print()
+
+        option = input('Please enter an opiton: ')
+        if option == '1':
+            password = input('Please enter a password: ')
+            password = encode(password)
+            print('Your password has been encoded and stored!')
+
+        elif option == '2':
+            print(f'The encoded password is {password}, and the original password is {decode(password)}')
+        elif option == '3':
+            exit()
+
+        print()
+
+if __name__ == '__main__':
+    main()
+
